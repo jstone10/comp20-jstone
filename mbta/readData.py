32afb78944b3deb054d2183a4fe6ddd38b46149c
Josh stone <joshastone@gmail.com>
2018-06-14 00:23:06 -0400

    a whole lot of mtba work

--- mbta/readData.py
+++ mbta/readData.py
@@ -3,12 +3,21 @@ import json
 
 data = open('redstations.txt', 'r')
 
+
 labels = data.readline()
 labels = [item.strip() for item in (labels.strip()).split('|')]
-print(labels)
 junk = data.readline()
 
+stations = []
 
 for line in data:
     col = [item.strip() for item in (line.strip()).split('|')]
+    station = {}
+    for i in range(len(labels)):
+        station[labels[i]] = col[i]
+    stations.append(station)
+data.close()
 
+f = open('stations', 'w')
+
+jsonData = json.dump(stations, f)
