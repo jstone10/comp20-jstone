e9578d8160304fb665b9375afcc70a6cbf262fb8
Josh stone <joshstone@Joshs-MacBook-Pro.local>
2018-06-15 13:49:53 -0400

    first draft of messages lab

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,26 @@
 // Your JavaScript goes here...
+
+function loadMessages() {
+    //create request instance
+    request = new XMLHttpRequest();
+
+    //set up HTTP REQUEST
+    request.open("GET", "./data.json", true);
+
+    //set up response
+    request.onreadystatechange = function() {
+        if (request.readystate == 4 && request.status == 200) {
+            messagesDiv = document.getElementById("messages");
+
+            theString = request.responseText;
+
+            messages = JSON.parse(theString);
+
+            outputString = "";
+            for (i = 0; i < 2; i++) {
+                outputString += "<p>" + messages[i]["content"] + "<sub> " + messages[i]["username"] + "</sub></p>";
+            }
+            messagesDiv.innerHTML = outputString;
+        }
+    };
+}
